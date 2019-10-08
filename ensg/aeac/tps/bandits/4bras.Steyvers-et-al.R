c797ae41753d77834b3f6d1871122488a2d1c79f
Preux Philippe <philippe.preux@univ-lille1.fr>
2019-10-08 08:57:20 +0200

    replace link by file iteslf

--- ensg/aeac/tps/bandits/4bras.Steyvers-et-al.R
+++ ensg/aeac/tps/bandits/4bras.Steyvers-et-al.R
@@ -1,1 +1,14 @@
-/home/ppreux/ensg/m1psycho/aeac/mim/2012-2013/bras/4bras.Steyvers-et-al.R
+#
+# 4bras.Steyvers-et-al.R
+#
+# Oct. 2013
+#
+
+source ("http://www.grappa.univ-lille3.fr/~ppreux/ensg/aeac/tps/bandits/4bras.Steyvers-et-al.fct.R")
+
+if (system ("logname", intern = T) == "ppreux") {
+  session.Steyvers.et.al (2, 20)
+} else {
+  session.Steyvers.et.al (20, 20)
+}
+
