4abbb127c8b1741dc00e738c3ba06b978312271f
PREUX Philippe <philippe.preux@inria.fr>
2024-08-14 13:41:29 +0200

    update

---
+++ ensg/m2-info/PDI/tp/alea/barplot.h
@@ -0,0 +1,8 @@
+/*
+ * barplot.h
+ */
+
+void barplot (const double *, const double *, const size_t);
+void plot_series (double **, const size_t, const size_t, const int, const char *);
+void plot_2d (const double *, const double *, const size_t, const char *);
+void scatter_plot (double **, const size_t, const char *);
